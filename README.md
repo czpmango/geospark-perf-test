3d2da28599285707789dcaaf1c8164de57830047
czp <zhipeng.cao@zilliz.com>
2020-04-18 19:27:30 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Dependencies :
+ - sbt
+ - scala-2.11
+ - spark-2.4.4
+
+# compile && run test
+CMD :
+```bash
+- modify your spark-bin path
+bash ./run-geospark.sh
+```
--- run-geospark.sh
+++ run-geospark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm ./target/scala-2.11/GeoSparkSQLScalaTemplate-assembly-0.1.0.jar
 sbt assembly
-#/home/czp/workspace/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class GeoSparkDemo ./target/scala-2.11/GeoSparkSQLScalaTemplate-assembly-0.1.0.jar 
-/home/czp/workspace/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class GeoSparkTest ./target/scala-2.11/GeoSparkSQLScalaTemplate-assembly-0.1.0.jar -h -p 1 -o 2 -f ST_Point -t 5 
+${spark_home}/bin/spark-submit --class GeoSparkTest ./target/scala-2.11/GeoSparkSQLScalaTemplate-assembly-0.1.0.jar -h -p 1 -o 2 -f ST_Point -t 5 
 
